7a93561e572bcafbfcd45d68fbc1dbd458814839
fedya <fedya.3.4.6.2.7@gmail.com>
2025-12-29 20:10:40 +0200

    edit main.py

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import sys
-import os
-from convert import convert_to_wav
 from transcribe import transcribe_audio
 from split import split_text
-from config import INPUT_FILE, TEMP_WAV
+from config import INPUT_FILE
 
 def main():
     input_file = sys.argv[1] if len(sys.argv) > 1 else INPUT_FILE
-    convert_to_wav(input_path=input_file)
-    transcribe_audio()
+    transcribe_audio(input_path=input_file)
     split_text()
-    os.remove(TEMP_WAV)
 
 if __name__ == "__main__":
     main()
