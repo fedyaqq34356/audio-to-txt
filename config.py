fe1370cb8619ac9cea5ad18bec9ca6eba7917e09
fedya <fedya.3.4.6.2.7@gmail.com>
2026-01-14 23:31:56 +0200

    edit config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-MODEL_SIZE = "medium"
-COMPUTE_TYPE = "float32"
+MODEL_SIZE = "large"
+COMPUTE_TYPE = "int8"
 DEVICE = "cpu"
 INPUT_FILE = "audio.mp3"
 OUTPUT_FILE = "audio.txt"
@@ -7,4 +7,7 @@ LOG_FILE = "transcription.log"
 CHUNK_SIZE = 500
 TEMP_WAV = "temp_audio.wav"
 LANGUAGE = None
-SUPPORTED_FORMATS = ['.mp3', '.mp4', '.avi', '.mkv', '.mov', '.wmv', '.flv', '.webm', '.m4a', '.wav', '.aac', '.ogg', '.flac']
+SUPPORTED_FORMATS = [
+    '.mp3', '.wav', '.m4a', '.aac', '.ogg', '.flac', '.wma', '.opus', '.alac', '.ape', '.aiff', '.amr', '.oga', '.spx', '.tta', '.wv', '.mka',
+    '.mp4', '.avi', '.mkv', '.mov', '.wmv', '.flv', '.webm', '.mpeg', '.mpg', '.m4v', '.3gp', '.3g2', '.f4v', '.vob', '.ogv', '.ts', '.mts', '.m2ts'
+]
